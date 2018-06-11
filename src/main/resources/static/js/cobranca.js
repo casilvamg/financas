cdb44d2e0356271222a0d646b6ac392a53615987
Carlos <casilvamg@hotmail.com>
2018-06-11 18:10:41 -0300

    'correção_delete_investimento'

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -2,17 +2,19 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 	var button = $(event.relatedTarget);
 	
 	var codigoTitulo = button.data('codigo');
-	var descricaoTitulo = button.data('descricao');
+	var descricao = button.data('descricao');
+	
+	var pathname = window.location.pathname; // Retorna só o path
 	
 	var modal = $(this);
-	var form = modal.find('form');
-	var action = form.data('url-base');
-	if (!action.endsWith('/')) {
-		action += '/';
-	}
-	form.attr('action', action + codigoTitulo);
+	var form = modal.find('form');	
 	
-	modal.find('.modal-body span').html('Tem certeza que deseja excluir o registro <strong>' + descricaoTitulo + '</strong>?');
+	if (!pathname.endsWith('/')) {
+		pathname += '/';
+	}
+	form.attr('action', pathname + codigoTitulo);
+	
+	modal.find('.modal-body span').html('Tem certeza que deseja excluir o registro <strong>' + descricao + '</strong>?');
 });
 
 $('#confirmacaoEmailModal').on('show.bs.modal', function(event) {
